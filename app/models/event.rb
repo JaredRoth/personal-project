deab86b8dd9fccf8c8c4d3f0fb56a7e295a50683
Jared Roth <jared.roth@gmail.com>
2016-06-08 22:31:47 -0600

    Implement event methods to filter events for view index
    Not tested yet... because I am bad

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,19 @@
 class Event < ActiveRecord::Base
   validates :name, presence: true
-  validates :year, presence: true, uniqueness: true
   validates :season, presence: true
+  validates :date, presence: true
+  # validates :name, uniqueness: {scope: :date}
+
+  def self.this_year
+    start = Date.yesterday
+    finish = Date.new(start.year + 1, 1, 1)
+    where(date: start..finish)
+  end
+
+  def self.next_year
+    date = Date.today
+    start = Date.new(date.year + 1, 1, 1)
+    finish = date.next_year
+    where(date: start..finish)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608213331) do
+ActiveRecord::Schema.define(version: 20160609040252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 20160608213331) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
-    t.integer  "year"
     t.string   "season"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "date"
   end
 
   create_table "vendors", force: :cascade do |t|
