c6c1e9cd874199088655de7a75b9d47fe7d1074c
Jared Roth <jared.roth@gmail.com>
2016-06-09 20:18:51 -0600

    Restore factory girl before suite lint

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,7 +2,12 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
   end
 
   config.before(:each) do
