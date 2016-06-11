9e5c65f4a3ac85307994199b1272e10bc09797d0
Jared Roth <jared.roth@gmail.com>
2016-06-11 10:42:51 -0600

    Removed some 'moisture' from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 class SessionsController < ApplicationController
   def create
     @vendor = Vendor.o_auth_find_or_create_by(request.env["omniauth.auth"])
+    session[:vendor_id] = @vendor.id
     if @vendor.is_new
-      session[:vendor_id] = @vendor.id
       redirect_to edit_vendor_path
     else
-      session[:vendor_id] = @vendor.id
       flash[:notice] = "Successfully Logged In"
       redirect_to root_path
     end
