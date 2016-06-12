a14d856610e02b52c4944826f24bc01ae8ebc312
Jared Roth <jared.roth@gmail.com>
2016-06-12 17:47:11 -0600

    Add droopdown button for profile/logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def link_to_login_or_logout
     if current_user
-      link_to "Logout", logout_path, method: :delete
+      render partial: "partials/dropdown"
     else
       link_to "Login", github_login_path
     end
